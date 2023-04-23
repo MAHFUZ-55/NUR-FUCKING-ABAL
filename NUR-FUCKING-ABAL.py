43381a8bd41672eed093f213fd4c8eb322ec0418
MAHFUZ-55 <127038327+MAHFUZ-55@users.noreply.github.com>
2023-04-23 10:28:33 +0600

    Add files via upload

---
+++ NUR-FUCKING-ABAL.py
@@ -0,0 +1,59 @@
+import os
+os.system("clear")
+
+logo = ("""
+\033[31;1m                   
+███    ███  █████  ██   ██ ███████ ██    ██ ███████ 
+████  ████ ██   ██ ██   ██ ██      ██    ██    ███  
+██ ████ ██ ███████ ███████ █████   ██    ██   ███   
+██  ██  ██ ██   ██ ██   ██ ██      ██    ██  ███    
+██      ██ ██   ██ ██   ██ ██       ██████  ███████ 
+                                                
+\033[32;1m                  
+\033[33;1m                 
+\033[34;1m                
+\033[35;1m               
+\033[32;1m           ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+\033[32;1m           ┃\033[34;1m TOOLS NAME => FUCK NUR             ┃
+\033[32;1m           ┃\033[32;1m OWNER NAME => MD MAHFUZ HASAN          ┃
+\033[32;1m           ┃\033[33;1m CLONE SYS  => DATA , WIFI              ┃
+\033[32;1m           ┃\033[32;1m WHATS APP  => +88017****         ┃
+\033[32;1m           ┃\033[35;1m FACEBOOK   => Md Mahfuz Hasan    ┃
+\033[32;1m           ┃\033[31;1m      PLEASE DON'T UES A RONG WORK      ┃
+\033[32;1m           ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛""")
+
+print(logo)
+print("[1] FUCK NUR")
+print("[2] FUCK NUR SISTER")
+print("[3] FUCK NUR MATHER")
+print("[4] FUCK YOUR**** NUR")
+
+x = input('CHOICE ')
+if x in ['1']:
+	os.system("clear")
+	print(logo)
+	print("PLZ FUCK NUR& NUR SISTER & NUR MATHER 💋💋 FUCK NUR SCRIPT OPEN SOURCE  https://github.com/MAHFUZ-55/Fuck_NUR")
+	
+x = input('NUR FUCK COMPLITE ✅ ')
+	
+if x in ['2']:
+	os.system("clear")
+	print(logo)
+	print("PLZ FUCK NUR & NUR SISTER & NUR MATHER 💋💋 FUCK NUR SCRIPT OPEN SOURCE  https://github.com/MAHFUZ-55/Fuck_NUR")
+	
+x = input('NUR FUCK COMPLITE ✅ ')
+	
+if x in ['3']:
+	os.system("clear")
+	print(logo)
+	print("PLZ FUCK NUR & NUR SISTER & NUR MATHER 💋💋 FUCK NUR SCRIPT OPEN SOURCE  https://github.com/MAHFUZ-55/Fuck_NUR")
+	
+x = input('NUR FUCK COMPLITE ✅ ')
+	
+if x in ['4']:
+	os.system("clear")
+	print(logo)
+	print("PLZ FUCK NUR & NUR SISTER & NUR MATHER 💋💋 FUCK NUR SCRIPT OPEN SOURCE  https://github.com/MAHFUZ-55/Open-Source")
+	
+x = input('NUR FUCK COMPLITE ✅ ')
+		
